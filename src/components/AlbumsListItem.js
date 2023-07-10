ba59bc3604627502e458182226e8e8ce2eb85206
kazimpashayev <kazimpasayev@protonmail.com>
2023-07-10 04:43:55 +0400

    first commit

---
+++ src/components/AlbumsListItem.js
@@ -0,0 +1,32 @@
+import { useRemoveAlbumMutation } from '../store';
+import { GoTrashcan } from 'react-icons/go';
+import Button from './Button';
+import ExpandablePanel from './ExpandablePanel';
+
+function AlbumsListItem({ album }) {
+  const [removeAlbum, results] = useRemoveAlbumMutation();
+
+  const handleRemoveAlbum = () => {
+    removeAlbum(album);
+  };
+
+  const header = (
+    <>
+      <Button
+        className="mr-2"
+        loading={results.isLoading}
+        onClick={handleRemoveAlbum}
+      >
+        <GoTrashcan />
+      </Button>
+      {album.title}
+    </>
+  );
+  return (
+    <ExpandablePanel key={album.id} header={header}>
+      List of Photos in the Album
+    </ExpandablePanel>
+  );
+}
+
+export default AlbumsListItem;
